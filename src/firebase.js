f2b793d479a436e45e08aa1fb266c8b85c829d86
Zulfikar Azmi <zazmialghifari@gmail.com>
2025-08-24 13:02:39 +0700

    config: remove Firebase Storage and add line-clamp CSS

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+// Hapus import storage
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAvu9yEikVXZFGK9HBvCTbTLR61TjGl6A",
@@ -17,4 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Hapus export storage
